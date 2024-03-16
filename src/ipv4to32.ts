302f84d47a349ce81be6b8e2ca5bc248d5684af5
Christian Amaya <amayarivasc@gmail.com>
2024-03-16 23:40:36 +0000

    ipv4 added

---
+++ src/ipv4to32.ts
@@ -0,0 +1,9 @@
+export function IPv4ToInt32(ip: string): number {
+  const octets = ip.split('.').map(Number)
+  const result =
+    ((octets[0] << 24) | (octets[1] << 16) | (octets[2] << 8) | octets[3]) >>> 0
+
+  return result
+}
+
+console.log(IPv4ToInt32('1.1.1.1'))
