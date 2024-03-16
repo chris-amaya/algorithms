fa0b41b54743e57e9ecd11468d48f6a564927e74
Christian Amaya <amayarivasc@gmail.com>
2024-03-16 20:19:09 +0000

    setup jest, typescript, prettier

--- src/time.ts
+++ src/time.ts
@@ -1,26 +1,18 @@
-const d = 'fsadf'
+function padding(date: number): string {
+  return date.toString().padStart(2, '0')
+}
 
-const a = {
-    arrowParens: 'always',
-    bracketSameLine: false,
-    bracketSpacing: true,
-    semi: true,
-    experimentalTernaries: false,
-    singleQuote: true,
-    jsxSingleQuote: false,
-    quoteProps: 'as-needed',
-    trailingComma: 'all',
-    singleAttributePerLine: false,
-    htmlWhitespaceSensitivity: 'css',
-    vueIndentScriptAndStyle: false,
-    proseWrap: 'preserve',
-    insertPragma: false,
-    printWidth: 80,
-    requirePragma: false,
-    tabWidth: 5,
-    useTabs: false,
-    embeddedLanguageFormatting: 'auto',
-    rangeStart: 0,
-    rangeEnd: 0,
-    cursorOffset: 0,
+export function humanReadable(seconds: number): string {
+  if (seconds >= 359999) {
+    throw new Error('Input exceeds maximum time limit')
+  }
+
+  const hours = Math.floor(seconds / 3600)
+  seconds %= 3600
+
+  const minutes = Math.floor(seconds / 60)
+
+  seconds %= 60
+
+  return `${padding(hours)}:${padding(minutes)}:${padding(seconds)}`
 }
